9659fb0339ba3cbd43c30983095981c46f16426f
shideard <ardiani_deshi@apps.ipb.ac.id>
2026-09-21 15:08:36 +0700

    feat: use UUID for users

--- backend/backend-epss/app/Models/User.php
+++ backend/backend-epss/app/Models/User.php
@@ -7,11 +7,12 @@ use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, HasUuids;
 
     /**
      * The attributes that are mass assignable.
